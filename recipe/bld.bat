a4073af1f1740f0e63775dc432932c179fc7320d
Michael Sarahan <msarahan@gmail.com>
2016-03-20 11:35:19 -0500

    use PKG_VERSION in windows recipe copying

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,7 +7,7 @@ set ARCH=Win32
 devenv.exe FreeImage.2008.sln /Upgrade
 msbuild FreeImage.2008.sln /property:Configuration=Release /property:Platform=%ARCH%
 
-copy Dist\FreeImage.dll %LIBRARY_BIN%\FreeImage-3.17.0.dll
+copy Dist\FreeImage.dll %LIBRARY_BIN%\FreeImage-%PKG_VERSION%.dll
 copy Dist\FreeImage.dll %LIBRARY_BIN%\FreeImage.dll
 copy Dist\FreeImage.h %LIBRARY_INC%\FreeImage.h
 copy Dist\FreeImage.lib %LIBRARY_LIB%\FreeImage.lib
